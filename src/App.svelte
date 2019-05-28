fa004ac9e92370d73a26920a9e89e3284180a606
Brian Sunter <bsunter@gmail.com>
2019-05-27 18:06:21 -0700

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,126 @@
+<script>
+export let name;
+export let appId = '775737172';
+let promise = getReviews({appId:'775737172',page:2});
+import cloud from 'd3-cloud';
+import  nGram from 'n-gram';
+const { trigram, bigram } = nGram;
+import { select } from 'd3-selection';
+import _ from 'lodash';
+import mimir from 'mimir';
+
+const {tfidf, tokenize } = mimir;
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+ const stopWords = new Set(["ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than","it","a","lot","of","i","used","tto","would","as","well","app","way","best","able","using","nice","thank","like","work","the","want","really","back","try","seems","could","see","use","one","also","get","need","cant","first","good","looking","trying","needs","last","old","doesn","much","new","great","ive","ve","easy","dont","change","bought","purchased"]);
+
+
+ const stops = new Set(["ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than","it","m"]);
+ function drawCloud(words) {
+
+     select("#cloud").selectAll("svg").remove();
+     select("#cloud").append("svg")
+       .attr("width", screenWidth)
+       .attr("height", screenHeight)
+       .append("g")
+       .attr("transform", "translate(" + screenWidth / 2 + "," + screenHeight / 2 + ")")
+       .selectAll("text")
+       .data(words)
+       .enter().append("text")
+       .style("font-size", function(d) { return d.size + "px"; })
+       .style("font-family", "Impact")
+       .attr("text-anchor", "middle")
+       .attr("transform", function(d) {
+           return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+       })
+       .text(function(d) { return d.text; });
+ }
+
+ function calculateCloud(data) {
+     cloud()
+         .size([screenWidth, screenHeight])
+         .words(data)
+         .padding(5)
+         .rotate(function() { return ~~(Math.random() * 2) * 90; })
+         .font("Impact")
+         .fontSize(function(d) { return d.size; })
+         .on("end", drawCloud).start();
+}
+
+
+ async function getReviews({appId}) {
+     var reviews = [];
+
+     for (var page = 1; page < 10 ; page++) {
+         const res = await fetch(`https://itunes.apple.com/us/rss/customerreviews/id=${appId}/sortBy=mostRecent/page=${page}/json`);
+         if (res.ok ) {
+             const jsonResponse = await res.json();
+             if (jsonResponse.feed.entry){
+                 const convertedReviews = jsonResponse.feed.entry.map(r => ({rating:r["im:rating"].label, title: r.title.label, content:r.content.label}))
+                 reviews.push(convertedReviews);
+
+             }
+         } else {
+             throw new Error(jsonResponse);
+         }
+
+
+}
+
+     return {reviews: reviews.flat()};
+ }
+ function isSuperset(set, subset) {
+     for (var elem of subset) {
+         if (!set.has(elem)) {
+             return false;
+         }
+     }
+     return true;
+}
+ function handleClick() {
+     promise = getReviews({appId,page:1}).then(r=> {
+
+         let grams = r.reviews.map(x => bigram(tokenize(x.content).filter(y=> !stops.has(y))).filter(tg => !isSuperset(stopWords,new Set(tg)))).flat();
+
+         let allReviews = r.reviews.map(x=>tokenize(x.content).join(' '));
+
+         /* let allReviewsText = r.reviews.map(x => tokenize(x.content)).flat(); */
+         let counted = _.countBy(grams);
+         let sorted  = _.sortBy(Object.keys(counted), o => counted[o] * -1 );
+
+
+         let d3Text = [];
+         sorted.slice(0,1000).forEach(s => {
+             /* console.log(s.split(',') + counted[s]); */
+             let phrase = s.split(',').join(' ');
+
+             /* console.log(tfidf('foo', allReviewsText, allReviews)); */
+             d3Text.push({text: phrase, size:counted[s] * 3 })
+         })
+         calculateCloud(d3Text);
+     }).catch(console.log);
+ }
+
+handleClick();
+</script>
+
+<style>
+ h1 {
+     color: purple;
+ }
+</style>
+
+<h1>App Reviews </h1>
+
+<button on:click={handleClick}>
+  Get App Reviews
+</button>
+
+<input bind:value={appId}>
+
+{#await promise then value}
+<!-- <p>the value is {value}</p> -->
+{/await}
+
+<div id="cloud"> </div>
