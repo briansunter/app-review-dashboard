cfae997bcaa90d86848a7970ec1693699c0b3320
Brian Sunter <bsunter@gmail.com>
2019-06-02 17:57:15 -0700

    Dashboard Component

---
+++ src/Dashboard.svelte
@@ -0,0 +1,100 @@
+<script>
+ export let currentApp;
+ export let currentAppReviewsSentiment;
+</script>
+
+<div class="row">
+  <div class="col-lg-3 col-md-6 col-sm-6">
+    <div class="card card-stats">
+      <div class="card-header card-header-success card-header-icon">
+        <div class="card-icon">
+          <i class="material-icons">star</i>
+        </div>
+        <p class="card-category">Current Version</p>
+        {#if currentApp}
+        <h3 class="card-title">{currentApp.currentVersionRating}
+          <small>Stars</small>
+        </h3>
+        {:else}
+        <p>Loading...</p>
+        {/if}
+      </div>
+      <div class="card-footer">
+        {#if currentApp}
+        <p class="card-category"> Out of {currentApp.currentVersionRatingCount} Reviews. </p>
+        {:else}
+        <p>Loading...</p>
+        {/if}
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 col-sm-6">
+    <div class="card card-stats">
+      <div class="card-header card-header-warning card-header-icon">
+        <div class="card-icon">
+          <i class="material-icons">rate_review</i>
+        </div>
+        <p class="card-category">Average Rating</p>
+        {#if currentApp}
+        <h3 class="card-title">{currentApp.appRating}
+          <small>Stars</small>
+        </h3>
+        {:else}
+        <p>Loading...</p>
+        {/if}
+      </div>
+      <div class="card-footer">
+        {#if currentApp}
+        <p class="card-category"> Out of {currentApp.appRatingCount} Reviews. </p>
+        {:else }
+        <p>Loading...</p>
+        {/if}
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 col-sm-6">
+    <div class="card card-stats">
+      <div class="card-header card-header-success card-header-icon">
+        <div class="card-icon">
+          <i class="material-icons">sentiment_satisfied</i>
+        </div>
+        <p class="card-category">Sentiment</p>
+
+        {#if currentAppReviewsSentiment}
+        <h3 class="card-title">{currentAppReviewsSentiment.toString().substring(0,6)}</h3>
+        {:else}
+        <h3 class="card-category">Loading...</h3>
+        {/if}
+      </div>
+      <div class="card-footer">
+        {#if currentApp}
+        <p class="card-category"> Out of {currentApp.appRatingCount} Reviews. </p>
+        {:else}
+        <p>Loading...</p>
+        {/if}
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-3 col-md-6 col-sm-6">
+    <div class="card card-stats">
+      <div class="card-header card-header-success card-header-icon">
+        <div class="card-icon">
+          <i class="material-icons">store</i>
+        </div>
+        {#if currentApp}
+        <p class="card-category">{currentApp.name.substring(0,20)}</p>
+        <h3 class="card-title">{currentApp.appPrice}</h3>
+        {:else }
+        <p>Loading...</p>
+        {/if}
+      </div>
+      <div class="card-footer">
+        {#if currentApp}
+        <a class="card-category" href={currentApp.link}>Go to store</a>
+        {:else}
+        <p>Loading...</p>
+        {/if}
+      </div>
+    </div>
+  </div>
+</div>
--- src/Home.svelte
+++ src/Home.svelte
@@ -3,10 +3,13 @@
  import meanBy from 'lodash/meanBy';
  import WordCloud from './WordCloud.svelte';
  import AutoComplete from './AutoComplete.svelte';
+ import Dashboard from './Dashboard.svelte';
+
  import { getApp, getReviews, formatReviews, reviewsSentiment } from './reviews';
 
  import 'material-dashboard/assets/css/material-dashboard.css';
  import 'material-dashboard/assets/css/bootstrap.min.css';
+
  const defaultAppId = 284882215;
  export let appId = 284882215;
 
@@ -77,107 +80,12 @@
       </form>
     </div>
   </div>
+  {#await Promise.all([currentApp,currentAppReviewsSentiment])}
+  <Dashboard />
+  {:then val}
+  <Dashboard currentApp={val[0]} currentAppReviewsSentiment={val[1]} />
+  {/await}
 
-  <div class="row">
-
-    <div class="col-lg-3 col-md-6 col-sm-6">
-      <div class="card card-stats">
-        <div class="card-header card-header-success card-header-icon">
-          <div class="card-icon">
-            <i class="material-icons">star</i>
-          </div>
-          <p class="card-category">Current Version</p>
-
-          {#await currentApp}
-          <p>Loading...</p>
-          {:then value}
-          <h3 class="card-title">{value.currentVersionRating}
-            <small>Stars</small>
-          </h3>
-          {/await}
-        </div>
-        <div class="card-footer">
-          {#await currentApp}
-          <p>Loading...</p>
-          {:then value}
-          <p class="card-category"> Out of {value.currentVersionRatingCount} Reviews. </p>
-          {/await}
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6">
-      <div class="card card-stats">
-        <div class="card-header card-header-warning card-header-icon">
-          <div class="card-icon">
-            <i class="material-icons">rate_review</i>
-          </div>
-          <p class="card-category">Average Rating</p>
-          {#await currentApp}
-          <p>Loading...</p>
-          {:then value}
-
-          <h3 class="card-title">{value.appRating}
-            <small>Stars</small>
-          </h3>
-          {/await}
-        </div>
-        <div class="card-footer">
-          {#await currentApp}
-          <p>Loading...</p>
-          {:then value}
-          <p class="card-category"> Out of {value.appRatingCount} Reviews. </p>
-          {/await}
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6">
-      <div class="card card-stats">
-        <div class="card-header card-header-success card-header-icon">
-          <div class="card-icon">
-            <i class="material-icons">sentiment_satisfied</i>
-          </div>
-          <p class="card-category">Sentiment</p>
-
-          {#await currentAppReviewsSentiment}
-          <h3 class="card-category">Loading...</h3>
-          {:then value}
-          <h3 class="card-title">{value.toString().substring(0,6)}</h3>
-          {:catch error}
-          <p>Something went wrong: {error.message}</p>
-          {/await}
-        </div>
-        <div class="card-footer">
-          {#await currentApp}
-          <p>Loading...</p>
-          {:then value}
-          <p class="card-category"> Out of {value.appRatingCount} Reviews. </p>
-          {/await}
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-6 col-sm-6">
-      <div class="card card-stats">
-        <div class="card-header card-header-success card-header-icon">
-          <div class="card-icon">
-            <i class="material-icons">store</i>
-          </div>
-          {#await currentApp}
-          <p>Loading...</p>
-          {:then value}
-          <p class="card-category">{value.name.substring(0,20)}</p>
-          <h3 class="card-title">{value.appPrice}</h3>
-          {/await}
-        </div>
-        <div class="card-footer">
-          {#await currentApp}
-          <p>Loading...</p>
-          {:then value}
-          <a class="card-category" href={value.link}>Go to store </a>
-          {/await}
-        </div>
-      </div>
-    </div>
-  </div>
 </div>
 
 {#await formattedReviews}
