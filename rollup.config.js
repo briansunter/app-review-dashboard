4185fb6f262e9e2719cb54b26ed29f72aa8fcb4c
Brian Sunter <bsunter@gmail.com>
2019-07-14 16:11:50 -0700

    Purge unused CSS

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 // import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 // import visualizer from 'rollup-plugin-visualizer';
 import serve from 'rollup-plugin-serve';
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 const production = !process.env.ROLLUP_WATCH;
 function onwarn (warning, warn) {
@@ -28,20 +28,23 @@ export default {
         css.write('public/bundle.css');
       }
     }),
-
     postcss({
       extensions: ['.css'],
       extract: 'public/vendor.css',
       minimize: production,
+      plugins:[
+      production && purgecss({
+        content: ["./**/*.html", "./**/*.svelte"],
+        css: ['public/vendor.css','public/bundle.css']
+      })]
     }),
-
     resolve(),
     commonjs(),
 
     // !production && livereload('public'),
     !production && serve({contentBase:'public',   historyApiFallback: true,
 }),
-    production && terser()
+    production
   ],
   watch: {
     clearScreen: false
